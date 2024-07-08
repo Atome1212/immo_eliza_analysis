d14e006c6ece35663d96c971b107141c64d8b140
AlisherJard <alisherjard@gmail.com>
2024-07-08 16:18:20 +0200

    updated cleaning_data_Alisher.py (working on Denis's json data)

---
+++ data/cleaning_data_Alisher.py
@@ -0,0 +1,112 @@
+import pandas as pd
+from scipy import stats
+
+file = '/home/becode/becode/immo_eliza_analysis/data/formatted_json.json'
+
+df = pd.read_json(file)
+
+
+print(f'{df.shape} number of rows and columns')
+
+print('\n')
+
+print(df.info())
+
+print('\n')
+
+print('Missing values:')
+print(df.isnull().sum())
+print(df['GardenArea'].mean())
+
+print('\n')
+
+df_copy = df.copy()
+df_copy.drop('Url', axis=1, inplace=True)
+print('Number of duplicates:')
+print(df_copy.duplicated().sum())
+print(df_copy.shape)
+print('\n')
+
+print('Ensuring all data types are correct:')
+print(df.dtypes)
+import pandas as pd
+from scipy import stats
+
+file = '/home/becode/becode/immo_eliza_analysis/data/formatted_json.json'
+
+df = pd.read_json(file)
+
+
+print(f'{df.shape} number of rows and columns')
+
+print('\n')
+
+print(df.info())
+
+print('\n')
+
+print('Missing values:')
+print(df.isnull().sum())
+print(df['GardenArea'].mean())
+
+print('\n')
+
+df_copy = df.copy()
+df_copy.drop('Url', axis=1, inplace=True)
+print('Number of duplicates:')
+print(df_copy.duplicated().sum())
+
+print('\n')
+
+print('Ensuring all data types are correct:')
+print(df.dtypes)
+
+print('\n')
+
+
+z_scores = stats.zscore(df.select_dtypes(include=[int,float]))
+outliers = (abs(z_scores)>3).sum()
+print(outliers)
+
+print('\n')
+
+print(df['GardenArea'].value_counts())
+
+print('\n')
+
+df.fillna({'GardenArea': 0}, inplace=True)
+df.fillna({'ConstructionYear': 0}, inplace=True)
+df.fillna({'Kitchen': 0}, inplace=True)
+
+print('\n')
+print(df.isnull().sum())
+print(df['GardenArea'].mean())
+
+print('\n')
+
+
+z_scores = stats.zscore(df.select_dtypes(include=[int,float]))
+outliers = (abs(z_scores)>3).sum()
+print(outliers)
+
+print('\n')
+
+print(df['GardenArea'].value_counts())
+
+print('\n')
+
+df.fillna({'GardenArea': 0}, inplace=True)
+df.fillna({'ConstructionYear': 0}, inplace=True)
+df.fillna({'Kitchen': 0}, inplace=True)
+
+print('\n')
+print(df.isnull().sum())
+
+
+b = []
+for x in df['GardenArea']:
+    if x > 0:
+        b.append(x)
+
+print(sum(b)/len(b))
+print(df['GardenArea'].mean())
