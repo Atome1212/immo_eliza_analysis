da038ff39b458a6283ea911219daeca0425282c2
AlisherJard <alisherjard@gmail.com>
2024-07-08 16:46:52 +0200

    updated cleaning_data_Alisher.py (working on Denis's json data)

--- data/cleaning_data_Alisher.py
+++ data/cleaning_data_Alisher.py
@@ -24,37 +24,9 @@ df_copy = df.copy()
 df_copy.drop('Url', axis=1, inplace=True)
 print('Number of duplicates:')
 print(df_copy.duplicated().sum())
-print(df_copy.shape)
-print('\n')
-
-print('Ensuring all data types are correct:')
-print(df.dtypes)
-import pandas as pd
-from scipy import stats
-
-file = '/home/becode/becode/immo_eliza_analysis/data/formatted_json.json'
-
-df = pd.read_json(file)
-
-
-print(f'{df.shape} number of rows and columns')
-
-print('\n')
-
-print(df.info())
-
-print('\n')
-
-print('Missing values:')
-print(df.isnull().sum())
-print(df['GardenArea'].mean())
-
-print('\n')
-
-df_copy = df.copy()
-df_copy.drop('Url', axis=1, inplace=True)
-print('Number of duplicates:')
-print(df_copy.duplicated().sum())
+for index, x in enumerate(df_copy.duplicated()):
+    if x is True:
+        print(index)
 
 print('\n')
 
@@ -68,15 +40,6 @@ z_scores = stats.zscore(df.select_dtypes(include=[int,float]))
 outliers = (abs(z_scores)>3).sum()
 print(outliers)
 
-print('\n')
-
-print(df['GardenArea'].value_counts())
-
-print('\n')
-
-df.fillna({'GardenArea': 0}, inplace=True)
-df.fillna({'ConstructionYear': 0}, inplace=True)
-df.fillna({'Kitchen': 0}, inplace=True)
 
 print('\n')
 print(df.isnull().sum())
@@ -84,20 +47,21 @@ print(df['GardenArea'].mean())
 
 print('\n')
 
-
-z_scores = stats.zscore(df.select_dtypes(include=[int,float]))
-outliers = (abs(z_scores)>3).sum()
-print(outliers)
-
-print('\n')
-
 print(df['GardenArea'].value_counts())
 
 print('\n')
 
+
+df = df.drop([df.index[15775], df.index[15776]])
 df.fillna({'GardenArea': 0}, inplace=True)
 df.fillna({'ConstructionYear': 0}, inplace=True)
 df.fillna({'Kitchen': 0}, inplace=True)
+df.fillna({'StateOfBuilding': 0}, inplace=True)
+df.fillna({'Heating': 0}, inplace=True)
+df.fillna({'SurfaceOfGood': 0}, inplace=True)
+df.fillna({'LivingArea': 0}, inplace=True)
+df.fillna({'NumberOfFacades': 0}, inplace=True)
+
 
 print('\n')
 print(df.isnull().sum())
@@ -110,3 +74,5 @@ for x in df['GardenArea']:
 
 print(sum(b)/len(b))
 print(df['GardenArea'].mean())
+
+
